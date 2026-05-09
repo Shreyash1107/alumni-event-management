b7602e19b3442b34351f7e3f238c4f4548478364
Shreyash Kawane <shreyunkawane@gmail.com>
2026-05-09 15:50:42 +0530

    Created the Mapper for Register Module

--- management/src/main/java/com/alumniEvent/management/mapper/RegisterMapper.java
+++ management/src/main/java/com/alumniEvent/management/mapper/RegisterMapper.java
@@ -1,4 +1,21 @@
 package com.alumniEvent.management.mapper;
 
+import com.alumniEvent.management.dto.RegisterDto;
+import com.alumniEvent.management.entity.RegisterEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class RegisterMapper {
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public RegisterEntity dtoToEntity(RegisterDto registerDto){
+        return modelMapper.map(registerDto, RegisterEntity.class);
+    }
+
+    public RegisterDto entityToDto(RegisterEntity registerEntity){
+        return modelMapper.map(registerEntity, RegisterDto.class);
+    }
 }
