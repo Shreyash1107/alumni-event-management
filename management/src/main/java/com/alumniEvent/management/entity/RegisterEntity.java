6b7e9b2d764b1be5afee0e8a8d70c7ab05b1d6e9
Shreyash Kawane <shreyunkawane@gmail.com>
2026-05-09 15:44:56 +0530

    Working on Register Module

--- management/src/main/java/com/alumniEvent/management/entity/RegisterEntity.java
+++ management/src/main/java/com/alumniEvent/management/entity/RegisterEntity.java
@@ -1,11 +1,9 @@
 package com.alumniEvent.management.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @ToString
 @NoArgsConstructor
